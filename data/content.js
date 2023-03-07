fc8b567a1ee435a7aba63f78ca54d9edf54130d7
Evan Christopher <evanchristopher1907@gmail.com>
2023-03-07 21:13:23 +0100

    initial content data

---
+++ data/content.js
@@ -0,0 +1,123 @@
+const userData = {
+    name: "Evan Christopher",
+    address: "Munich, Germany",
+}
+
+const jobExperience = [
+    {
+        role: "Working Student: Software Developer",
+        company: "Giesecke+Devrient",
+        startDate: "11.2021",
+        endDate: "Present",
+        tasks: [
+            "Software Only Tool for KMS (Key Management System): Worked on an internal application for the management and testing of cryptographic keys in HSMs",
+        ],
+        technologies: [
+            "Angular",
+            "Spring Boot",
+        ]
+    },
+]
+
+const projects = [
+    {
+        title: "iPraktikum: Themis App",
+        description: "In my 5th semester I got accepted to participate in the famous iPraktikum, a practical course which has been offered by the Research group for Applied Software Engineering which consisted of about 60 developers (Bachelor’s and Master’s students) divided into 8 different teams.\n\nI was part of the CIT Team which developed an open-source iOS application for tutors to grade programming assignments and exams as an extension of the well-known interactive learning platform Artemis. It features smart feedback suggestions with machine learning and allows tutors to scribble feedback using a stylus.",
+        links: {
+            github: "https://github.com/ls1intum/Themis",
+            homepage: "https://ase.in.tum.de/lehrstuhl_1/teaching/1201-ipraktikum-ws22-23"
+        },
+        technologies: [
+            "Swift/UIKit",
+            "Xcode",
+            "Spring Boot"
+        ]
+    },
+    {
+        title: "JavaScript Technology Seminar: Genomics.ai",
+        description: "In my 4th semester I got accepted to the JavaScript Technology Seminar which consisted of about 27 developers (Bachelor’s and Master’s students) divided into 5 teams who together developed a web application for the Helmholtz Institute to annotate and visualise single-cell sequencing data using neural networks. The project has since been\n\nI was part of the team which enables the upload single-cell annotation data for the visualisation of sequencing data using neural networks (e.g. for cancer research).",
+        links: {
+            github: "https://github.com/jst-seminar-rostlab-tum/genomics.ai",
+            homepage: "https://www.rostlab.org/owiki/index.php/Javascript_Technology_Seminar_List_of_Selected_Projects#Winter_term_2021.2F2022_.26_Summer_term_2022_-_GeneCruncher"
+        },
+        technologies: [
+            "React",
+            "D3.js",
+            "MUI (Material-UI)",
+        ]
+    },
+    {
+        title: "Virtual Sportwest Promotional Website",
+        description: "A promotional website made for an event held by the Indonesian Student Association in Munich.",
+        links: {
+            github: "https://github.com/Ferdydh/vsportfest",
+            homepage: "https://vsportfest.ppi-munich.org/"
+        },
+        technologies: [
+            "React",
+            "MUI (Material-UI)",
+            "D3.js"
+        ]
+    },
+    {
+        title: "ReGenerator",
+        description: "A Recipe Generator powered by OpenAPI's GPT-3, made as a submission for my CS50 Intro to Computer Science final project.",
+        links: {
+            github: "https://github.com/crustypluto19/ReGenerator",
+            demo: "https://youtu.be/AfDlBpJLNK0"
+        },
+        technologies: [
+            "React",
+            "TailwindCSS",
+            "OpenAI's GPT-3"
+        ]
+    }
+]
+
+const skills = {
+    programmingLanguages: [
+        "Java",
+        "JavaScript / Typescript",
+        "HTML / CSS",
+        "Swift",
+        "Python",
+        "R"
+    ],
+    frameworks: [
+        "React",
+        "Spring Boot",
+        "Next.js",
+        "Angular"
+    ],
+    libraries: [
+        "TailwindCSS",
+        "Material UI",
+        "Chakra UI",
+        "D3.js",
+    ],
+    devOps: [
+        "Git",
+        "GitHub",
+        "Docker",
+    ],
+    databases: [
+        "MySQL",
+        "MongoDB",
+    ],
+    tools: [
+        "VSCode",
+        "IntelliJ",
+        "Xcode",
+        "Postman",
+        "Figma",
+        "Jira",
+        "Confluence",
+        "Linear"
+    ],
+    languages: [
+        "English",
+        "German",
+        "Indonesian",
+        "Mandarin"
+    ]
+}
